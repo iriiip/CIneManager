aa7c1ac10fd080f913efd71f54be936fd5f46818
iriiip <uriel.lopez.vidal@ces-vegamedia.es>
2026-01-17 20:24:29 +0100

    Modelo y migración inicial

---
+++ database/migrations/2026_01_17_190203_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('synopsis');
+            $table->integer('duration');
+            $table->integer('age');
+            $table->foreignId('genre_id')->constrained('genres')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
